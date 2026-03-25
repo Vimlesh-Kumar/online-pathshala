e6b08b5f0f94f0b421d33fedd59f0118add8ea0a
Vimlesh Kumar <vimlesh11072000@gmail.com>
2026-03-26 02:32:00 +0530

    style(ui): force light defaultTheme and fix global search css

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -15,6 +15,7 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
+    defaultTheme: 'light',
     themes: {
       light: {
         colors: {
